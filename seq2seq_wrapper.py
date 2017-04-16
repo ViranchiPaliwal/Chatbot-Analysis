07d292f04d05beba9f8d17fb54a30369a4d5790e
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2017-04-16 18:41:51 -0400

    minor change

--- seq2seq_wrapper.py
+++ seq2seq_wrapper.py
@@ -142,7 +142,7 @@ class Seq2Seq(object):
         for i in range(self.epochs):
             try:
                 self.train_batch(sess, train_set)
-                if i!=0 &&  i/5000  == 0: # TODO : make this tunable by the user
+                if i!=0 and  i/5000  == 0: # TODO : make this tunable by the user
                     # save model to disk
                     saver.save(sess, self.ckpt_path + self.model_name + '.ckpt', global_step=i)
                     # evaluate to get validation loss
